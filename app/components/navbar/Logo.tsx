4e70a4fe7797b90b6904c332affb9bfde8308970
Dimas Septyanto <samid77@Dimass-MacBook-Pro.local>
2023-05-24 14:29:44 +0700

    Update on navbar

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,19 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import {useRouter} from 'next/navigation'
+
+const Logo = () => {
+    const router = useRouter();
+    return (
+        <Image 
+            className='hidden md:block cursor-pointer'
+            alt='Airbnb logo'
+            src='/images/logo.png'
+            height={'100'}
+            width={'100'}
+        />
+    )
+}
+
+export default Logo
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import React from 'react'
 import Container from '../Container'
+import Logo from './Logo'
+import Search from './Search'
+import UserMenu from './UserMenu'
 
 const Navbar = () => {
   return (
@@ -7,7 +12,9 @@ const Navbar = () => {
         <div className='py-4 border-b-[1px]'>
             <Container>
                 <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
-                    
+                    <Logo />
+                    <Search />
+                    <UserMenu />
                 </div>
             </Container>
         </div>
