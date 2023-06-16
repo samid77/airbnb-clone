26b1b9252c3144bad614ca737c5acc1469d533dc
Dimas Septyanto <samid77@Dimass-MacBook-Pro.local>
2023-06-16 18:55:31 +0700

    Setup prisma and finish sign up form using complex tailwindcss

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -89,7 +89,7 @@ if(!isOpen) return null;
                         <div className='relative p-6 flex-auto'>
                             {body}
                         </div>
-                        <div className='f lex flex-col gap-2 p-6'>
+                        <div className='flex flex-col gap-2 p-6'>
                             <div className='flex flex-row items-center gap-4 w-full'>
                                 {
                                     secondaryAction && secondaryActionLabel && (
@@ -107,6 +107,7 @@ if(!isOpen) return null;
                                     onClick={handleSubmit}
                                 />
                             </div>
+                            {footer}
                         </div>
                     </div>
                 </div>
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -6,6 +6,10 @@ import {FcGoogle} from 'react-icons/fc';
 import {Field, FieldValues, SubmitHandler, useForm} from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
+import Heading from '../Heading';
+import Input from '../inputs/Input';
+import { toast } from 'react-hot-toast';
+import Button from '../Button';
 
 const RegisterModal = () => {
 
@@ -33,12 +37,74 @@ const RegisterModal = () => {
         registerModal.onClose();
     })
     .catch((err) => {
-        console.log(err)
+        toast.error('Something went wrong')
     })
     .finally(() => {
         setIsLoading(false)
     });
   }
+
+  const bodyContent = (
+    <div className='flex flex-col gap-4'>
+        <Heading 
+            title='Welcome to Airbnb'
+            subtitle='Create an account!'
+        />
+        <Input 
+            id='email'
+            label='Email'
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+        />
+        <Input 
+            id='name'
+            label='Name'
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+        />
+        <Input 
+            id='password'
+            type={'password'}
+            label='Password'
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+        />
+    </div>
+  )
+
+  const footerContent = (
+    <div className='flex flex-col gap-4 mt-3'>
+        <hr />
+        <Button 
+            outline
+            label='Continue with Google'
+            icon={FcGoogle}
+            onClick={() => {}}
+        />
+        <Button 
+            outline
+            label='Continue with Github'
+            icon={AiFillGithub}
+            onClick={() => {}}
+        />
+        <div className='text-neutral-500 text-center mt-4 font-light'>
+            <div className='justify-center flex flex-row items-center gap-2'>
+                <div>
+                    Already have an account ?
+                </div>
+                <div onClick={registerModal.onClose} className='text-neutral-800 cursor-pointer hover:underline'>
+                    Log In
+                </div>
+            </div>
+        </div>
+    </div>
+  )
 
   return (
     <Modal 
@@ -48,6 +114,8 @@ const RegisterModal = () => {
         actionLabel='Continue'
         onClose={registerModal.onClose}
         onSubmit={handleSubmit(onSubmit)}
+        body={bodyContent}
+        footer={footerContent}
     />
   )
 }
